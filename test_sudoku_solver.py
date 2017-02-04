85427d63600fe2570c9cc5071ebf2336684f5aa1
EmilioOldenziel <emilio.oldenziel@gmail.com>
2017-02-04 01:11:49 +0100

    python 3 prints

--- test_sudoku_solver.py
+++ test_sudoku_solver.py
@@ -13,13 +13,13 @@ def main():
 
         result = recognize_sudoku.recognize(image_name)
         if sudoku == result:
-            print image_name + ': pass'
+            print(image_name + ': pass')
         else:
-            print image_name + ': fail'
+            print(image_name + ': fail')
             
-            print 'test input:'
-            print sudoku_solver.print_sudoku(sudoku)
-            print 'result:'
-            print sudoku_solver.print_sudoku(result)
+            print('test input:')
+            print(sudoku_solver.print_sudoku(sudoku))
+            print('result:')
+            print(sudoku_solver.print_sudoku(result))
 
 main()
